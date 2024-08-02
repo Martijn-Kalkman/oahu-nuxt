31678d37be0bf154056479a7c76107d12c5aaef1
Martijn-Kalkman <kalkmanwm@gmail.com>
2024-08-02 23:00:30 +0200

    tours/act

--- pages/Activities.vue
+++ pages/Activities.vue
@@ -49,7 +49,7 @@
       <Card
         title="CLASSIC SEATING"
         image="/classicseating.jpg"
-        description="Join our ʻOhana for an exciting evening under the stars as we take you on a voyage across Ka Moana (the ocean)..."
+        description="Join our ʻOhana for an exciting evening under the stars as we take you on a voyage across Ka Moana (the ocean). Witness world-class entertainment at the best luau on Oahu, that begins and ends with fire!"
         price="$129.00"
         duration="2 Hours 15 Minutes"
         foodProvided="Food Provided"
@@ -58,7 +58,8 @@
       <Card
         title="Magnum Helicopters"
         image="/helicopteradventure.png"
-        description="This tour immerses you in the natural beauty of Oahu..."
+        description="This tour immerses you in the natural beauty of Oahu.
+          Flying with the doors off the aircraft will give you a spectacular unobstructed view of the island’s best landmarks and attractions."
         price="$329.00"
         duration="50 Minutes"
         foodProvided="No Food Provided"
@@ -67,7 +68,7 @@
       <Card
         title="Experience Nutridge"
         image="/experiencenutridge.png"
-        description="This is a unique combination of Hawaiian history and culture through hands-on excursions..."
+        description="This is a unique combination of Hawaiian history and culture through hands-on excursions. Come up to our historical estate and allow your senses to be engaged during this hands-on, interactive tour."
         price="$180.00"
         duration="4 Hours"
         foodProvided="Food Provided"
@@ -76,7 +77,7 @@
       <Card
         title="Jetski Oahu"
         image="/jetski.png"
-        description="Get out on the water and ride along the beautiful blue waters of Waikiki Beach!"
+        description="Get out on the water and ride along the beautiful blue waters of Waikiki Beach! Our state-of-the-art Jet Skis have the speed and capability to skim across the water, and easy to use for any first-time riders."
         price="$110.00"
         duration="1 Hour 15 Minutes"
         foodProvided="No Food Provided"
@@ -85,7 +86,7 @@
       <Card
         title="X-treme Parasail"
         image="/xtremeparasail.png"
-        description="Up to 1,000 feet of tow line allows you and a friend to enjoy perfect views of Diamond Head..."
+        description="Up to 1,000 feet of tow line allows you and a friend to enjoy perfect views of Diamond Head from Oahu’s south shore. Choose the length that will satisfy your craving for adventure: 700, 850, 900  or 1,000 feet."
         price="$75.00"
         duration="1 Hour"
         foodProvided="No Food Provided"
@@ -94,7 +95,7 @@
       <Card
         title="Hawaii Shark Encounters"
         image="/sharkcagediving.png"
-        description="This tour takes you into the open ocean, over three miles offshore, where you can observe sharks..."
+        description="This tour takes you into the open ocean, over three miles offshore, where you can observe Galapagos and Sandbar sharks in their natural environment. A floating cage keeps humans and sharks safely separate."
         price="$125.00"
         duration="2 Hours"
         foodProvided="No Food Provided"
@@ -103,7 +104,7 @@
       <Card
         title="Mauka Warriors Luau - Koa Classic Package: Rear Seating"
         image="/rearseating.png"
-        description="Join us at Mauka Warriors Luau for an authentic experience of traditional culture and history..."
+        description="Join us at Mauka Warriors Luau for an authentic experience of traditional culture and history, overlooking the spectacular Honolulu skyline and Diamond Head. "
         price="$139.00"
         duration="3 Hours 15 Minutes"
         foodProvided="Food Provided"
@@ -112,11 +113,29 @@
       <Card
         title="Mauka Warriors Luau - Ali’i Premium Package: Midsection Seating"
         image="/midseating.png"
-        description="Join us at Mauka Warriors Luau for an authentic experience of traditional culture and history..."
+        description="Join us at Mauka Warriors Luau for an authentic experience of traditional culture and history, overlooking the spectacular Honolulu skyline and Diamond Head. "
         price="$169.00"
         duration="3 Hours 15 Minutes"
         foodProvided="Food Provided"
         bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/559365/calendar/2024/07/?flow=no&full-items=yes"
+      />
+      <Card
+          title="Mauka Warriors Luau - Mōʻī Royal Package: Front Seating"
+          image="/frontseating.png"
+          description="Join us at Mauka Warriors Luau for an authentic experience of traditional culture and history, overlooking the spectacular Honolulu skyline and Diamond Head.  "
+          price="$199.00"
+          duration="3 Hours 15 Minutes"
+          foodProvided="Food Provided"
+          bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/559223/calendar/2024/07/?flow=no&full-items=yes"
+      />
+      <Card
+          title="Three Tikis Sailing - Sail & Snorkel"
+          image="/frontseating.png"
+          description="Embark on a mesmerizing journey through the crystal clear waters of Hawaii. Get ready to witness an underwater wonderland of tropical fish and majestic sea turtles, and sail past iconic Diamond Head."
+          price="$99.00"
+          duration="2 Hours"
+          foodProvided="No Food Provided"
+          bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/561910/calendar/2024/07/?flow=no&full-items=yes"
       />
     </div>
   </div>
--- pages/Tours.vue
+++ pages/Tours.vue
@@ -23,189 +23,62 @@
   </section>
   <!--  Section cards-->
   <section>
-    <div class="flex lg:flex-row flex-col bg-[#EBE9E2] justify-center items-center">
-<!--        Card 1-->
-      <div data-aos="fade-up" data-aos-duration="1500" class="xl:w-[380px] lg:w-[500px] md:w-[600px] md:mx-2 bg-white relative lg:mt-24 mt-14 mx-8 lg:mx-0 pb-8 lg:pb-0">
-        <img draggable="false" class="z-10 w-full h-[200px] object-cover" src="/waimeawaterfalls.png" alt="">
-        <div class="absolute lg:top-32 top-36 left-4 bg-[#96B53D] px-3 py-2">
-          <h2 class="text-white Bebas text-2xl">Full Day Oahu Tour with Byodo Temple & Waimea Waterfalls</h2>
-        </div>
-        <div>
-          <p class="h-[210px] Montserratreg text-sm mx-8 mt-4 leading-[3] mb-4 text-center lg:text-left">The ultimate circle island tour with TWO premium sites, Waimea Waterfall and Byodo-In Temple!</p>
-          <div class="border-b-2 mx-8 border-black mb-4"></div>
-        </div>
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 flex flex-col lg:ml-8 mb-8">
-            <ul class="lg:ml-4 Montserratreg text-xs lg:text-left text-center">
-              <li class="p-2"><span class="text-l font-bold">• </span><span class="font-bold text-l">$149.00</span> per person</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>8-9 Hours</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>No Food Provided</li>
-            </ul>
-          </div>
-          <div class="lg:w-1/2 flex flex-col items-center">
-            <a
-                class="w-[150px] text-center border border-[#F58C1F] border-2 bg-[#F58C1F] px-4 py-2 rounded-3xl text-white text-l font-semibold hover:bg-white hover:border-[#F58C1F] hover:text-[#F58C1F] mb-4"
-                href="https://fareharbor.com/embeds/book/thetourconsultants/items/559776/calendar/2024/07/?flow=no&full-items=yes">BOOK NOW
-            </a>
-            <div class="flex flex-row">
-              <img draggable="false" src="/phoneblack.png" alt="phone icon">
-              <p class="Aileron font-bold ml-4">(808) 562-3222</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--      Card 2-->
-      <div data-aos="fade-up" data-aos-duration="1500" class="xl:w-[380px] lg:w-[500px] md:w-[600px] lg:ml-4 bg-white relative lg:mt-24 mt-14 mx-8 xl:ml-16 lg:mr-0 pb-8 lg:pb-0">
-        <img draggable="false" class="z-10 w-full h-[200px] object-cover" src="/pineappleexp.png" alt="">
-        <div class="absolute lg:top-32 top-36 left-4 bg-[#96B53D] px-3 py-2">
-          <h2 class="text-white Bebas text-2xl">The Pineapple EXP - Circle Island Tour</h2>
-        </div>
-        <div>
-          <p class="h-[210px] Montserratreg text-sm mx-8 mt-4 leading-[3] mb-4 text-center lg:text-left">We at The Pineapple EXP want to make sure you have the best experience here in Hawaii without paying the world known Hawaii high prices. This is why the company was founded; to truly spread aloha.</p>
-          <div class="border-b-2 mx-8 border-black mb-4 mb-4 text-center lg:text-left"></div>
-        </div>
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 flex flex-col lg:ml-8 mb-8">
-            <ul class="lg:ml-4 Montserratreg text-xs lg:text-left text-center">
-              <li class="p-2"><span class="text-l font-bold">• </span><span class="font-bold text-l">$143.00</span> per person</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>9 Hour tour</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>No Food Provided</li>
-            </ul>
-          </div>
-          <div class="lg:w-1/2 flex flex-col items-center">
-            <a
-                class="w-[150px] text-center border border-[#F58C1F] border-2 bg-[#F58C1F] px-4 py-2 rounded-3xl text-white text-l font-semibold hover:bg-white hover:border-[#F58C1F] hover:text-[#F58C1F] mb-4"
-                href="https://fareharbor.com/embeds/book/thetourconsultants/items/561556/calendar/2024/07/?flow=no&full-items=yes">BOOK NOW
-            </a>
-            <div class="flex flex-row">
-              <img draggable="false" src="/phoneblack.png" alt="phone icon">
-              <p class="Aileron font-bold ml-4">(808) 562-3222</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!--      Card 3-->
-      <div data-aos="fade-up" data-aos-duration="1500" class="xl:w-[380px] lg:w-[500px] md:w-[600px] lg:ml-4 bg-white relative lg:mt-24 mt-14 mx-8 xl:ml-16 lg:mr-0 pb-8 lg:pb-0 lg:mb-0 mb-16">
-        <img draggable="false" class="z-10 w-full h-[200px] object-cover" src="/halfdaypearlharbour.jpg" alt="">
-        <div class="absolute lg:top-32 top-36 left-4 bg-[#96B53D] px-3 py-2">
-          <h2 class="text-white Bebas text-2xl">HALF DAY PEARL HARBOR WITH USS ARIZONA MEMORIAL AND CITY TOUR</h2>
-        </div>
-        <div>
-          <p class="h-[210px] Montserratreg text-sm mx-8 mt-4 leading-[3] mb-4 text-center lg:text-left">A trip to Hawaii isn’t complete without a visit to the Pearl Harbor National Memorial Center and the USS Arizona Memorial, let us take you there!</p>
-          <div class="border-b-2 mx-8 border-black mb-4"></div>
-        </div>
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 flex flex-col lg:ml-8 mb-8">
-            <ul class="lg:ml-4 Montserratreg text-xs lg:text-left text-center">
-              <li class="p-2"><span class="text-l font-bold">• </span><span class="font-bold text-l">$55.00</span> per person</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>4-5 Hours tour</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>No Food Provided</li>
-            </ul>
-          </div>
-          <div class="lg:w-1/2 flex flex-col items-center">
-            <a
-                class="w-[150px] text-center border border-[#F58C1F] border-2 bg-[#F58C1F] px-4 py-2 rounded-3xl text-white text-l font-semibold hover:bg-white hover:border-[#F58C1F] hover:text-[#F58C1F] mb-4"
-                href="https://fareharbor.com/embeds/book/thetourconsultants/items/559773/calendar/2024/07/?flow=no&full-items=yes">BOOK NOW
-            </a>
-            <div class="flex flex-row">
-              <img draggable="false" src="/phoneblack.png" alt="phone icon">
-              <p class="Aileron font-bold ml-4">(808) 562-3222</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="flex lg:flex-row flex-col bg-[#EBE9E2] justify-center lg:pb-36 items-center">
-<!--      Card 4-->
-      <div data-aos="fade-up" data-aos-duration="1500" class="xl:w-[380px] lg:w-[500px] md:w-[600px] md:mx-2 bg-white relative lg:mt-24 mt-14 mx-8 lg:mx-0 pb-8 lg:pb-0">
-        <img draggable="false" class="z-10 w-full h-[200px] object-cover" src="/thecompleteexperiencefromoahu.jpg" alt="">
-        <div class="absolute lg:top-32 top-36 left-4 bg-[#96B53D] px-3 py-2">
-          <h2 class="text-white Bebas text-2xl">THE COMPLETE EXPERIENCE FROM OAHU</h2>
-        </div>
-        <div>
-          <p class="h-[210px] Montserratreg text-sm mx-8 mt-4 leading-[3] mb-4 text-center lg:text-left">Our Complete Pearl Harbor experience gives you the best, most personal overview of the United States entry into and subsequent commitment to the World War II effort. </p>
-          <div class="border-b-2 mx-8 border-black mb-4"></div>
-        </div>
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 flex flex-col lg:ml-8 mb-8">
-            <ul class="lg:ml-4 Montserratreg text-xs lg:text-left text-center">
-              <li class="p-2"><span class="text-l font-bold">• </span><span class="font-bold text-l">$194.99</span> per person</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>10 Hours</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>No Food Provided</li>
-            </ul>
-          </div>
-          <div class="lg:w-1/2 flex flex-col items-center">
-            <a
-                class="w-[150px] text-center border border-[#F58C1F] border-2 bg-[#F58C1F] px-4 py-2 rounded-3xl text-white text-l font-semibold hover:bg-white hover:border-[#F58C1F] hover:text-[#F58C1F] mb-4"
-                href="https://fareharbor.com/embeds/book/thetourconsultants/items/560833/calendar/2024/07/?flow=no&full-items=yes">BOOK NOW
-            </a>
-            <div class="flex flex-row">
-              <img draggable="false" src="/phoneblack.png" alt="phone icon">
-              <p class="Aileron font-bold ml-4">(808) 562-3222</p>
-            </div>
-          </div>
-        </div>
-      </div>
-<!--      Card 5-->
-      <div data-aos="fade-up" data-aos-duration="1500" class="xl:w-[380px] lg:w-[500px] md:w-[600px] lg:ml-4 bg-white relative lg:mt-24 mt-14 mx-8 xl:ml-16 lg:mr-0 pb-8 lg:pb-0">
-        <img draggable="false" class="z-10 w-full h-[200px] object-cover" src="/ussarizonamemorial.jpg" alt="">
-        <div class="absolute lg:top-32 top-36 left-4 bg-[#96B53D]  px-3 py-2">
-          <h2 class="text-white Bebas text-2xl">USS ARIZONA MEMORIAL AND HISTORIC HONOLULU TOUR</h2>
-        </div>
-        <div>
-          <p class="h-[210px] Montserratreg text-sm mx-8 mt-4 leading-[3] mb-4 text-center lg:text-left">Our journey takes you to the World War II Valor in the Pacific National Monument where the Pearl Harbor Visitor Center and the USS Arizona Memorial await you.</p>
-          <div class="border-b-2 mx-8 border-black mb-4"></div>
-        </div>
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 flex flex-col lg:ml-8 mb-8">
-            <ul class="lg:ml-4 Montserratreg text-xs lg:text-left text-center">
-              <li class="p-2"><span class="text-l font-bold">• </span><span class="font-bold text-l">$68.99</span> per person</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>2 Hours 30 Minutes</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>No Food Provided</li>
-            </ul>
-          </div>
-          <div class="lg:w-1/2 flex flex-col items-center">
-            <a
-                class="w-[150px] text-center border border-[#F58C1F] border-2 bg-[#F58C1F] px-4 py-2 rounded-3xl text-white text-l font-semibold hover:bg-white hover:border-[#F58C1F] hover:text-[#F58C1F] mb-4"
-                href="https://fareharbor.com/embeds/book/thetourconsultants/items/560839/calendar/2024/07/?flow=no&full-items=yes">BOOK NOW
-            </a>
-            <div class="flex flex-row">
-              <img draggable="false" src="/phoneblack.png" alt="phone icon">
-              <p class="Aileron font-bold ml-4">(808) 562-3222</p>
-            </div>
-          </div>
-        </div>
-      </div>
-<!--     Card 6-->
-      <div data-aos="fade-up" data-aos-duration="1500" class="xl:w-[380px] lg:w-[500px] md:w-[600px] lg:ml-4 bg-white relative lg:mt-24 mt-14 mx-8 xl:ml-16 lg:mr-0 pb-8 lg:pb-0 mb-36 lg:mb-0">
-        <img draggable="false" class="z-10 w-full h-[200px] object-cover" src="/pearlharbormemorial.jpg" alt="">
-        <div class="absolute lg:top-32 top-36 left-4 bg-[#96B53D] px-3 py-2">
-          <h2 class="text-white Bebas text-2xl">PEARL HARBOR MEMORIAL AND BATTLESHIP MISSOURI TOUR FROM OAHU</h2>
-        </div>
-        <div>
-          <p class="h-[210px] Montserratreg text-sm mx-8 mt-4 leading-[3] mb-4 text-center lg:text-left">Your friendly tour guide will drive you all around Honolulu in our very comfortable, Mercedes sprinter vans. We take you directly to Pearl Harbor where you will spend almost 4.5-hours exploring the areaʻs attractions.</p>
-          <div class="border-b-2 mx-8 border-black mb-4"></div>
-        </div>
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 flex flex-col lg:ml-8 mb-8">
-            <ul class="lg:ml-4 Montserratreg text-xs lg:text-left text-center">
-              <li class="p-2"><span class="text-l font-bold">• </span><span class="font-bold text-l">$144.99</span> per person</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>4 Hours 30 Minutes</li>
-              <li class="p-2"><span class="text-l font-bold">• </span>No Food Provided</li>
-            </ul>
-          </div>
-          <div class="lg:w-1/2 flex flex-col items-center">
-            <a
-                class="w-[150px] text-center border border-[#F58C1F] border-2 bg-[#F58C1F] px-4 py-2 rounded-3xl text-white text-l font-semibold hover:bg-white hover:border-[#F58C1F] hover:text-[#F58C1F] mb-4"
-                href="https://fareharbor.com/embeds/book/thetourconsultants/items/563912/calendar/2024/07/?flow=no&full-items=yes">BOOK NOW
-            </a>
-            <div class="flex flex-row">
-              <img draggable="false" src="/phoneblack.png" alt="phone icon">
-              <p class="Aileron font-bold ml-4">(808) 562-3222</p>
-            </div>
-          </div>
-        </div>
+    <div class="bg-[#EBE9E2]">
+      <div class="grid grid-cols-1 sm:grid-cols-2 p-8 lg:grid-cols-3 gap-4 container mx-auto  justify-center items-center">
+        <Card
+            title="Full Day Oahu Tour with Byodo Temple & Waimea Waterfalls"
+            image="/waimeawaterfalls.png"
+            description="The ultimate circle island tour with TWO premium sites, Waimea Waterfall and Byodo-In Temple!"
+            price="$149.00"
+            duration="8-9 Hours"
+            foodProvided="No Food Provided"
+            bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/559776/calendar/2024/07/?flow=no&full-items=yes"
+        />
+        <Card
+            title="The Pineapple EXP - Circle Island Tour"
+            image="/pineappleexp.png"
+            description="We at The Pineapple EXP want to make sure you have the best experience here in Hawaii without paying the world known Hawaii high prices. This is why the company was founded; to truly spread aloha."
+            price="$143.00"
+            duration="9 Hours"
+            foodProvided="No Food Provided"
+            bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/561556/calendar/2024/07/?flow=no&full-items=yes"
+        />
+        <Card
+            title="HALF DAY PEARL HARBOR WITH USS ARIZONA MEMORIAL AND CITY TOUR"
+            image="/halfdaypearlharbour.jpg"
+            description="A trip to Hawaii isn’t complete without a visit to the Pearl Harbor National Memorial Center and the USS Arizona Memorial, let us take you there!"
+            price="$55.00"
+            duration="4-5 Hours"
+            foodProvided="No Food Provided"
+            bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/559773/calendar/2024/07/?flow=no&full-items=yes"
+        />
+        <Card
+            title="THE COMPLETE EXPERIENCE FROM OAHU"
+            image="/thecompleteexperiencefromoahu.jpg"
+            description="Our Complete Pearl Harbor experience gives you the best, most personal overview of the United States entry into and subsequent commitment to the World War II effort."
+            price="$194.99"
+            duration="10 hours"
+            foodProvided="No Food Provided"
+            bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/560833/calendar/2024/07/?flow=no&full-items=yes"
+        />
+        <Card
+            title="USS ARIZONA MEMORIAL AND HISTORIC HONOLULU TOUR"
+            image="/ussarizonamemorial.jpg"
+            description="Our journey takes you to the World War II Valor in the Pacific National Monument where the Pearl Harbor Visitor Center and the USS Arizona Memorial await you."
+            price="$68.99"
+            duration="2 Hours 30 Minutes"
+            foodProvided="No Food Provided"
+            bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/560839/calendar/2024/07/?flow=no&full-items=yes"
+        />
+        <Card
+            title="PEARL HARBOR MEMORIAL AND BATTLESHIP MISSOURI TOUR FROM OAHU"
+            image="/pearlharbormemorial.jpg"
+            description="Your friendly tour guide will drive you all around Honolulu in our very comfortable, Mercedes sprinter vans. We take you directly to Pearl Harbor where you will spend almost 4.5-hours exploring the areaʻs attractions."
+            price="$144.99"
+            duration="4 Hours 30 Minutes"
+            foodProvided="No Food Provided"
+            bookingLink="https://fareharbor.com/embeds/book/thetourconsultants/items/563912/calendar/2024/07/?flow=no&full-items=yes"
+        />
       </div>
     </div>
 
