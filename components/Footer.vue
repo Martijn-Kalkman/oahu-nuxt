d4cd421549c6f375074727d2f483487ca710a455
Martijn-Kalkman <kalkmanwm@gmail.com>
2024-09-01 17:27:24 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,45 +1,140 @@
-<template>
-    <footer>
-      <div class="flex lg:flex-row flex-col">
-        <div class="relative lg:w-6/12">
-          <div class="relative">
-            <img class="w-full h-96 object-cover" draggable="false" src="/tours.png" alt="">
-            <div class="absolute inset-0 flex justify-center items-center lg:hidden">
-              <router-link  to="/tours" @click="scrollToTop"  class="bg-[#F58C1f] text-white py-4 px-6 hover:bg-white text-7xl hover:text-[#F58C1f] Bebas">TOURS</router-link>
-            </div>
-            <div class="absolute lg:right-0 lg:bottom-0 lg:mr-8  lg:mb-4 hidden lg:flex">
-              <div class="flex justify-center">
-                <router-link to="/tours" @click="scrollToTop" class="bg-[#F58C1f] hover:bg-white hover:text-[#F58C1f] text-white py-4 px-6  Bebas text-7xl ">TOURS</router-link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="relative lg:w-6/12">
-          <div class="relative">
-            <img class="w-full h-96 object-cover" draggable="false" src="/activities.png" alt="">
-            <div class="absolute inset-0 flex justify-center items-center lg:hidden">
-              <router-link to="/activities" @click="scrollToTop" class="bg-[#F58C1f] text-white py-4 text-7xl hover:bg-white hover:text-[#F58C1f] px-6  Bebas ">ACTIVITES</router-link>
-            </div>
-            <div class="absolute lg:left-0 lg:bottom-0 lg:ml-8 lg:mb-4 hidden lg:flex">
-              <div class="flex justify-center">
-                <router-link to="/activities" @click="scrollToTop" class="bg-[#F58C1f] text-white py-4 px-6 hover:bg-white hover:text-[#F58C1f] Bebas text-7xl">ACTIVITES</router-link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="bg-[#140B0B] h-[600px]">
-      </div>
-    </footer>
-  </template>
+<template>   
+    <footer class="bg-[#375D90] text-white pt-10">
+  <div class="container  mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
+    <!-- Company Info -->
+    <div>
+  <img src="/logooahu.png" alt="Hawaii Tours" class="mb-4 ml-12 " />
   
-  <script>
-  export default {
-    methods: {
-      scrollToTop() {
-        window.scrollTo(0, 0);
-      }
-    }
-  }
-  </script>
+  <!-- Address -->
+  <div class="flex items-center mb-2">
+    <Icon name="fa:map-marker" class="text-white mr-2 text-xl" />
+    <p class="text-l ">
+      55-541 Naniloa Loop,<br />
+      Laie, HI 96762
+    </p>
+  </div>
   
+  <!-- Phone -->
+  <div class="flex items-center mb-2">
+    <Icon name="material-symbols:phone-enabled-sharp" class="text-white mr-2 text-xl" />
+    <p class="text-sm">
+      <a href="tel:808-562-3222" class="text-xl font-bold  hover:underline">(808) 562-3222</a>
+    </p>
+  </div>
+  
+  <!-- Email -->
+  <div class="flex items-center mb-2">
+    <Icon name="material-symbols:mail-outline" class="text-white mr-2 text-xl" />
+    <p class="text-sm ">
+      <a href="mailto:info@hawaiitours.com" class="text-l hover:underline">info@gobeyondhawaii.com</a>
+    </p>
+  </div>
+  
+  <!-- Open Hours -->
+  <div class="flex items-center">
+    <Icon name="mdi:clock-outline" class="text-white mr-2 text-xl" />
+    <p class="text-l ">
+      <span class="font-bold">Open Daily</span><br />
+      5:00am to 9:00pm, HST
+    </p>
+  </div>
+
+  <!-- Social Media Icons -->
+  <div class="flex items-center mt-4 space-x-4">
+    <div class="flex items-center justify-center w-12 h-12 bg-white rounded-full">
+      <a href="#" class="flex items-center justify-center w-6 h-6">
+      <Icon name="brandico:facebook" class="text-[#375D90] w-full h-full" />
+    </a>
+  </div>
+  <div class="flex items-center justify-center w-12 h-12 bg-white rounded-full">
+    <a href="#" class="flex items-center justify-center w-6 h-6">
+      <Icon name="mdi:instagram" class="text-[#375D90] w-full h-full" />
+    </a>
+  </div>
+  <div class="flex items-center justify-center w-12 h-12 bg-white rounded-full">
+    <a href="#" class="flex items-center justify-center w-8 h-8">
+      <Icon name="mdi:youtube" class="text-[#375D90] w-full h-full" />
+    </a>
+  </div>
+</div>
+
+
+</div>
+
+
+
+    <!-- Most Popular Tours -->
+    <div>
+      <h3 class="text-lg font-semibold mb-4">Most Popular Tours</h3>
+      <ul class="text-sm space-y-2">
+        <li><a href="#" class="hover:underline">Pearl Harbor Tours</a></li>
+        <li><a href="#" class="hover:underline">Road to Hana Tours</a></li>
+       <li><a href="#" class="hover:underline">Haleakala Park Tours</a></li>
+        <li><a href="#" class="hover:underline">Inter-Island Day Trips</a></li>
+        <li><a href="#" class="hover:underline">Hawaii Volcano Tours</a></li>
+        <li><a href="#" class="hover:underline">Hawaiian Luaus</a></li>
+        <li><a href="#" class="hover:underline">Last Minute Hawaii Travel Deals</a></li>
+        <li><a href="#" class="hover:underline">Pride of America Ship Excursions</a></li>
+      </ul>
+    </div>
+
+    <!-- Private Tours -->
+    <div>
+      <h3 class="text-lg font-semibold mb-4">Private Tours</h3>
+      <ul class="text-sm space-y-2">
+        <li><a href="#" class="hover:underline">Oahu Private Tours</a></li>
+        <li><a href="#" class="hover:underline">Maui Private Tours</a></li>
+        <li><a href="#" class="hover:underline">Big Island Private Tours</a></li>
+        <li><a href="#" class="hover:underline">Kauai Private Tours</a></li>
+      </ul>
+
+      <h3 class="text-lg font-semibold mb-4 mt-8">Tours & Activities</h3>
+      <ul class="text-sm space-y-2">
+        <li><a href="#" class="hover:underline">All Tours & Activities</a></li>
+        <li><a href="#" class="hover:underline">Oahu Tours</a></li>
+        <li><a href="#" class="hover:underline">Maui Tours</a></li>
+        <li><a href="#" class="hover:underline">Big Island Tours</a></li>
+        <li><a href="#" class="hover:underline">Kauai Tours</a></li>
+        <li><a href="#" class="hover:underline">Hawaii Shore Excursions</a></li>
+      </ul>
+    </div>
+
+    <!-- About Hawaii Tours -->
+    <div>
+      <h3 class="text-lg font-semibold mb-4">About Hawaii Tours</h3>
+      <ul class="text-sm space-y-2">
+        <li><a href="#" class="hover:underline">About Us</a></li>
+        <li><a href="#" class="hover:underline">Sustainable Experiences</a></li>
+        <li><a href="#" class="hover:underline">Refunds & Cancellations</a></li>
+        <li><a href="#" class="hover:underline">Reviews</a></li>
+        <li><a href="#" class="hover:underline">Blogs</a></li>
+        <li><a href="#" class="hover:underline">Buy Gift Cards</a></li>
+        <li><a href="#" class="hover:underline">Contact Us</a></li>
+        <li><a href="#" class="hover:underline">Affiliates & Partners</a></li>
+        <li><a href="#" class="hover:underline">Maui Relief Efforts</a></li>
+        <li><a href="#" class="hover:underline">Group Travel Packages</a></li>
+      </ul>
+      <div class="flex items-center mt-4 space-x-4">
+        <!-- <img src="iata.png" alt="IATA" class="w-8"> -->
+        <!-- <img src="iatan.png" alt="IATAN" class="w-8"> -->
+      </div>
+    </div>
+  </div>
+  <div class="bg-[#6C95C4] mx-auto mt-8 border-t border-gray-700 py-4 text-sm">
+  <div class="container mx-auto flex flex-col md:flex-row justify-between items-center">
+    <!-- Left Side: Copyright -->
+    <p class="text-center md:text-left mb-4 md:mb-0">
+      © Copyright 2024 <a href="#" class="hover:underline">HAWAII TOURS</a>. All Rights Reserved.
+    </p>
+    
+    <!-- Right Side: Privacy and Cancellation Policies -->
+    <p class="text-center md:text-right">
+      <a href="#" class="hover:underline">Privacy Policy</a> | 
+      <a href="#" class="hover:underline">Cancellation Policy</a>
+    </p>
+  </div>
+</div>
+
+
+</footer>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/icon"],
   plugins: ['~/plugins/axios.js'], // Include the proxy plugin
   nitro:{
     plugins: ['~/server/index.ts'],
